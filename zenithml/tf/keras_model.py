09f47ffab0c333e4f6d0541808e6050718746e66
Praveen Chandar <praveenrchandar@gmail.com>
2022-05-11 15:43:22 -0400

    style: remove dead code & comments

--- zenithml/tf/keras_model.py
+++ zenithml/tf/keras_model.py
@@ -2,17 +2,6 @@ from abc import ABC
 from typing import Dict, Any, Optional
 
 import tensorflow as tf
-from tensorflow.keras.layers import Layer
-
-
-# def concat_layers(inputs, layers):
-#     dense_layer = []
-#     for input_col, pp in layers:
-#         if isinstance(input_col, list):
-#             dense_layer.append(pp([inputs[col] for col in input_col]))
-#         else:
-#             dense_layer.append(pp(inputs[input_col]))
-#     return tf.keras.layers.Concatenate()(dense_layer)
 
 
 def concat_layers(inputs, layers, split_str="__SPLIT__"):
@@ -32,8 +21,7 @@ def get_preprocess_layers_and_dims(pp_layers, split_str="__SPLIT__"):
     for group, layers in pp_layers.items():
         all_groups[group] = {split_str.join(k) if isinstance(k, list) else k: v for k, v in layers}
 
-    #input_dim = {k: sum([l.output_dim() for _, l in layers]) for k, layers in pp_layers.items()}
-    return all_groups#, input_dim
+    return all_groups
 
 
 class BaseKerasModel(tf.keras.Model, ABC):
